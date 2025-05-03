5890789eac31a09024282d981f58bc73fb1d8943
David-mwas <dmwas704@gmail.com>
2025-05-03 21:58:24 +0300

    ADD(CORE): weather api routes, weather/geocode,weather/current,weather/forecast

--- weather-api/routes/web.php
+++ weather-api/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WeatherController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/weather/geocode', [WeatherController::class, 'geocodeCity']);
+Route::get('/weather/current', [WeatherController::class, 'getCurrentWeather']);
+Route::get('/weather/forecast', [WeatherController::class, 'getForecast']);
