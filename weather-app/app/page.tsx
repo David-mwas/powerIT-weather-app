59a6d4b65e45553f36ded0024edfc9fb85c66036
David-mwas <dmwas704@gmail.com>
2025-05-04 14:19:03 +0300

    UPDATE page.tsx ->Refactor

--- weather-app/app/page.tsx
+++ weather-app/app/page.tsx
@@ -7,10 +7,6 @@ import CurrentWeather from "./components/CurrentWeather";
 
 import Forecast from "./components/Forecast";
 import WeatherStats from "./components/WeatherStats";
-// import SearchBar from "@/components/SearchBar";
-// import CurrentWeather from "@/components/CurrentWeather";
-// import Forecast from "@/components/Forecast";
-// import WeatherStats from "@/components/WeatherStats";
 
 export default function Home() {
   const [city, setCity] = useState("Nairobi");
@@ -19,6 +15,7 @@ export default function Home() {
   const [current, setCurrent] = useState(null);
   const [forecast, setForecast] = useState([]);
 
+  // fetchWeather function to get the weather data
   const fetchWeather = async () => {
     try {
       const geoRes = await fetch(
@@ -39,6 +36,7 @@ export default function Home() {
       const { lat, lon } = geoData[0];
       setLocation({ lat, lon });
 
+      // Fetch current and forecast weather data
       const [currentRes, forecastRes] = await Promise.all([
         fetch(
           `http://localhost:8000/api/weather/current?lat=${lat}&lon=${lon}&unit=${unit}`
@@ -55,7 +53,7 @@ export default function Home() {
         return;
       }
       const forecastData = await forecastRes.json();
-    
+
       console.log("Forecast data:", forecastData); // Add this
 
       setCurrent(currentData);
@@ -66,16 +64,16 @@ export default function Home() {
   };
 
   useEffect(() => {
+    // useEffect to fetch weather data when the component mounts or when city or unit changes
     fetchWeather();
   }, [city, unit]);
-  // console.log(forecast);
+
   return (
     <main className="p-4 max-w-4xl mx-auto space-y-6 h-[70vh]">
       <SearchBar city={city} setCity={setCity} unit={unit} setUnit={setUnit} />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-full">
         {current && <CurrentWeather data={current} city={city} />}
         <div className="h-full flex flex-col items-center justify-center">
-       
           {forecast && <Forecast data={forecast} />}
           {current && <WeatherStats data={current} />}
         </div>
