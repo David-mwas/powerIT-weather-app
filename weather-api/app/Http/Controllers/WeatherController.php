bcbc54533d2a9083081f9285f41e5d024262508a
David-mwas <dmwas704@gmail.com>
2025-05-04 14:07:32 +0300

    FIX(CORE): fix cors errors allow origins, add current,forecast,geocode endpoints->group cors middleware

--- weather-api/app/Http/Controllers/WeatherController.php
+++ weather-api/app/Http/Controllers/WeatherController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
+
 class WeatherController extends Controller
 {
     protected $apiKey;
--- weather-api/routes/web.php
+++ weather-api/routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Middleware\HandleCors;
 use App\Http\Controllers\WeatherController;
+use App\Http\Middleware\CustomCors;
 
-Route::get('/weather/geocode', [WeatherController::class, 'geocodeCity']);
-Route::get('/weather/current', [WeatherController::class, 'getCurrentWeather']);
-Route::get('/weather/forecast', [WeatherController::class, 'getForecast']);
+Route::middleware([CustomCors::class])->group(function () {
+    Route::get('/api/weather/geocode', [WeatherController::class, 'geocodeCity']);
+    Route::get('/api/weather/current', [WeatherController::class, 'getCurrentWeather']);
+    Route::get('/api/weather/forecast', [WeatherController::class, 'getForecast']);
+});
+
+
 
 // http://localhost:8000/api/weather/current?city=Nairobi&units=metric
